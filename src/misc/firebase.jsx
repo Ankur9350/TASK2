a5b0dd03426a1ccac66635a5bdab2399fda834f8
Sandesh Singh <sandeshsingh295@gmail.com>
2022-06-18 22:40:47 +0530

    scaffold the project

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,12 @@
+// import firebase from 'firebase/app';
+
+const Config = {
+  apiKey: 'AIzaSyAQSBNqw0wrVYlv06dC38pqkG49VEeel1s',
+  authDomain: 'chat-web-app-4cd82.firebaseapp.com',
+  projectId: 'chat-web-app-4cd82',
+  storageBucket: 'chat-web-app-4cd82.appspot.com',
+  messagingSenderId: '862160447144',
+  appId: '1:862160447144:web:87dd05189747935731c49d',
+};
+
+// const app = firebase.initializeApp(Config);
